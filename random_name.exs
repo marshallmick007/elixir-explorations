9f67c741ea9851c80d120fc7213cedf0c4520874
Marshall Mickelson <marshallmick+git@gmail.com>
2015-11-07 22:34:39 -0700

    Added new datafiles, implemented adverbs as well

--- random_name.exs
+++ random_name.exs
@@ -1,6 +1,9 @@
 # Seed Erlangs Random Number Gen
 :random.seed(:erlang.timestamp)
 
+# additional good files here: https://github.com/morganecf/synonym-game/tree/master/data/word_files
+
+adv_file = "adverbs-1.txt"
 adj_file = "adjectives-4.txt"
 noun_file = "nouns-4.txt"
 split_char = " "
@@ -10,14 +13,18 @@ read_file = fn(file) ->
   String.split(contents, ["\r", "\n"], trim: true)
 end
 
+adverbs = read_file.(adv_file)
 adjectives = read_file.(adj_file)
 nouns = read_file.(noun_file)
 
-IO.puts "Found #{length(adjectives)} Adjectives, #{length(nouns)} Nouns"
+IO.puts "Found #{length(adverbs)} Adverbs, #{length(adjectives)} Adjectives, #{length(nouns)} Nouns"
 
-adj1 = Enum.random(adjectives)
-adj2 = Enum.random(adjectives)
-noun = Enum.random(nouns)
+Enum.each(1..4, fn(_) ->
+  #adv1 = Enum.random(adverbs)
+  adj1 = Enum.random(adjectives)
+  adj2 = Enum.random(adjectives)
+  noun = Enum.random(nouns)
 
-IO.puts "#{adj1}#{split_char}#{adj2}#{split_char}#{noun}"
+  IO.puts "# #{adj1}#{split_char}#{adj2}#{split_char}#{noun}"
+end)
 
