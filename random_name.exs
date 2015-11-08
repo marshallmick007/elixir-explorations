b859a4f1c6cc5ee8383db02f1742864cebe2a7c9
Marshall Mickelson <marshallmick+git@gmail.com>
2015-11-07 22:14:34 -0700

    Code Cleanup

--- random_name.exs
+++ random_name.exs
@@ -1,24 +1,23 @@
 # Seed Erlangs Random Number Gen
-#:rand.seed(:exsplus, {0, 0, 7})
 :random.seed(:erlang.timestamp)
 
 adj_file = "adjectives-4.txt"
 noun_file = "nouns-4.txt"
-{:ok, contents} = File.read(adj_file)
-adjectives = String.split(contents, ["\r", "\n"], trim: true)
-{:ok, contents} = File.read(noun_file)
-nouns = String.split(contents, ["\r", "\n"], trim: true)
-#IO.puts contents
-#File.stream!(file) |> Enum.each( fn line ->
-  #list ++ line
-  #  IO.puts line
-  #end)
+split_char = " "
+
+read_file = fn(file) ->
+  {:ok, contents} = File.read(file)
+  String.split(contents, ["\r", "\n"], trim: true)
+end
+
+adjectives = read_file.(adj_file)
+nouns = read_file.(noun_file)
 
 IO.puts "Found #{length(adjectives)} Adjectives, #{length(nouns)} Nouns"
 
-adjective = Enum.random(adjectives)
-noun = Enum.random(nouns)
+adj1 = Enum.random(adjectives)
 adj2 = Enum.random(adjectives)
+noun = Enum.random(nouns)
 
-IO.puts "#{adjective} #{adj2} #{noun}"
+IO.puts "#{adj1}#{split_char}#{adj2}#{split_char}#{noun}"
 
